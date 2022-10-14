67da8262cbd04274a0a819c64b52d79c3b06c66e
igorslepenkov <slepenkov.nii@yandex.by>
2022-10-14 17:21:46 +0300

    feat(analizer): add title parsing functionality

--- app/services/capybara_services/parser.rb
+++ app/services/capybara_services/parser.rb
@@ -6,9 +6,17 @@ module CapybaraServices
 
     Capybara.run_server = false
 
-    def self.parse_article(url)
+    def self.parse_article(_url)
+      Capybara.register_driver :selenium do |app|
+        Capybara::Selenium::Driver.new(app, browser: :chrome)
+      end
+
       Capybara.current_driver = :selenium
-      visit(url)
+
+      visit('https://medium.com/yardcouch-com/youtube-is-dead-and-something-new-is-coming-132322b09be6')
+
+      title = find('.pw-post-title').text
+      puts title
     end
   end
 end
