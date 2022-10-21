12489ac59d98658e6b8dc7d8f1ca0db14239fbc1
igorslepenkov <slepenkov.nii@yandex.by>
2022-10-22 00:48:20 +0300

    deploy(dev.to-parser): add chrome binary to capybara intializer

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -5,16 +5,20 @@ Capybara.run_server = false
 Capybara.default_max_wait_time = 1
 Capybara.ignore_hidden_elements = false
 
-Capybara.register_driver :remote_selenium_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--window-size=1400,1400')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options:
-  )
-end
+chrome_opts =
+  Capybara.register_driver :remote_selenium_headless do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--window-size=1400,1400')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    chrome_bin ? options.binary = chrome_bin : nil
+
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options:
+    )
+  end
