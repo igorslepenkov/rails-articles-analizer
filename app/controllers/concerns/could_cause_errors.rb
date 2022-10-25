3ae1f594cb6a1f42371285f406a29e3ad901bb73
igorslepenkov <slepenkov.nii@yandex.by>
2022-10-25 13:55:36 +0300

    refactor(error-handling): delete StandardError handler

--- app/controllers/concerns/could_cause_errors.rb
+++ app/controllers/concerns/could_cause_errors.rb
@@ -2,20 +2,13 @@ module CouldCauseErrors
   extend ActiveSupport::Concern
 
   included do
-    rescue_from StandardError, with: :internal_server_error
-
     rescue_from NewslizerExceptions::DomainNotSupported, with: :domain_not_supported
   end
 
   private
 
   def domain_not_supported
-    flash[:alert] = "Sorry, the link, that you provided could not be processed, because its source is not suported"
-    redirect_to root_path and return
-  end
-
-  def internal_server_error
-    flash[:alert] = 'Sorry, it seems an unexpected error came from server, please try again later'
+    flash[:alert] = 'Sorry, the link, that you provided could not be processed, because its source is not suported'
     redirect_to root_path and return
   end
 end
