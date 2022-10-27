d90bf0cc00adabe67873fd5429ae23d83ef8c9a7
igorslepenkov <slepenkov.nii@yandex.by>
2022-10-27 16:35:33 +0300

    feat(tests): add status code check to cases, add test case when empty data has being returned from parser

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -9,9 +9,10 @@ RSpec.describe ArticlesController, type: :controller do
       expect(assigns(:articles)).to eq(articles)
     end
 
-    it 'should render the index template' do
+    it 'should render the index template with 200 status code' do
       get :index
       expect(response).to render_template('index')
+      expect(response).to have_http_status(200)
     end
   end
 
@@ -21,31 +22,48 @@ RSpec.describe ArticlesController, type: :controller do
       expect(assigns(:article)).to be_a_new(Article)
     end
 
-    it 'should render the new template' do
+    it 'should render the new template with 200 status code' do
       get :new
       expect(response).to render_template('new')
+      expect(response).to have_http_status(200)
     end
   end
 
   describe 'POST create' do
-    it 'should create new Article with valid url' do
-      post 'create', params: {article: {url: "https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes"}}
-      expect(Article.count).to eq 1
+    context 'when provided data is valid' do
+      it 'should create new Article with valid url' do
+        post 'create', params: { article: { url: 'https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes' } }
+        expect(Article.count).to eq 1
+      end
+  
+      it 'should redirect to article_path with 302 status code after successful creating' do
+        post 'create', params: { article: { url: 'https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes' } }
+        expect(response).to redirect_to articles_path
+        expect(response).to have_http_status(302)
+      end
     end
 
-    it 'should redirect to article_path after successful creating' do
-      post 'create', params: {article: {url: "https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes"}}
-      expect(response).to redirect_to articles_path
+    context 'when provided data is invalid' do
+      it 'should not create new Article with invalid url' do
+        post 'create', params: { article: { url: 'lalalalalalalaal' } }
+        expect(Article.count).to eq 0
+      end
+  
+      it 'should redirect to root_path with 302 status code, if domain is not supported' do
+        post 'create', params: { article: { url: 'https://medium.com/@james.a.hughes/using-the-reduce-method-in-ruby-907f3c18ae1f' } }
+        expect(response).to redirect_to root_path
+        expect(response).to have_http_status(302)
+      end
     end
 
-    it 'should not create new Article with invalid url' do
-      post 'create', params: {article: {url: "lalalalalalalaal"}}
-      expect(Article.count).to eq 0
-    end
-
-    it 'should redirect to root_path if domain is not supported' do
-      post 'create', params: {article: {url: "https://medium.com/@james.a.hughes/using-the-reduce-method-in-ruby-907f3c18ae1f"}}
-      expect(response).to redirect_to root_path
+    context 'when parser did not reponse with title and comments' do
+      it 'should render "new" with :bad_request status if article did not receive title from parser' do
+        allow_any_instance_of(ArticlesController).to receive(:article_params).and_return({url: 'https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes'})
+        allow_any_instance_of(ArticlesController).to receive(:get_parser_response).and_return({title: '', comments: []})
+        post 'create', params: { article: { url: 'https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes' } }
+        expect(response).to have_http_status(:bad_request)
+        expect(response).to render_template('new')
+      end
     end
   end
 end
